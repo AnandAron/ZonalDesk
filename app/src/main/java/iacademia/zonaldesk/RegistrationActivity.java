84974aa8fc6b05a01abfac91ba80a214395931d9
Anand <anandaron@gmail.com>
2017-06-22 14:26:42 +0530

    Fragments done
    Anand

--- app/src/main/java/iacademia/zonaldesk/RegistrationActivity.java
+++ app/src/main/java/iacademia/zonaldesk/RegistrationActivity.java
@@ -1,22 +1,15 @@
 package iacademia.zonaldesk;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.Shape;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    public static Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -26,12 +19,13 @@ public class RegistrationActivity extends AppCompatActivity {
         boolean firstInstall=p.getBoolean("FIRST_INSTALL",true);
         p.edit().putBoolean("FIRST_INSTALL",false).apply();
         boolean isVerified = p.getBoolean("VERIFICATION_STATUS",false);
+        c=this;
 
         //Only If the entry is not immediate after first install and if the user is verified , Redirect to the Home Activity
         //else continue with the Registration Activity
         //Note: Include the line "SharedPreferences.edit().putBoolean("VERIFICATION_STATUS",true).apply()" once the verification is done
         //Note that after entering to HomeActivity Registration Activity must be removed from the back stack by mentioning in the manifest file
-        if(!firstInstall & isVerified){
+        if(!firstInstall ){
             p.edit().putBoolean("VERIFICATION_STATUS",true).apply();//I ve included it here to make the app stable.
             // Remove this and update the status once the verification is done
             Intent i = new Intent(this,HomeActivity.class);
